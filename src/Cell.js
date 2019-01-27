46e201c09a3f1085928a9e15da481c471d8d049c
github username <github email address>
2019-01-26 21:00:52 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
-      color: this.props.color
+      color: this.props.color,
     }
   }
-  
+
+  handleClick = () => { //when you click anywhere, the cell will change color to the color you selected earlier.
+    // console.log("Clicking");
+    const selectedColor = this.props.getSelectedColor()
+    this.setState({
+      color: selectedColor
+    })
+  }
+
   render() {
+    console.log();
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
       </div>
     )
-  }
-  
+  }ß
 }
